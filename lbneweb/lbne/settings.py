373d976ad2d7f10f43939e9582742acd913e128c
Brett Viren <bv@bnl.gov>
2013-10-15 17:11:42 -0400

    Add authblk version of the author list and move the old one to files named to reflect its revtex4 nature.

--- lbneweb/lbne/settings.py
+++ lbneweb/lbne/settings.py
@@ -144,6 +144,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.core.context_processors.static",
     "django.core.context_processors.tz",
     "django.contrib.messages.context_processors.messages",
+    'django.core.context_processors.request',
     'lbne.context_processors.setting',
 )
 
